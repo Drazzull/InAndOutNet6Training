af2d15f059409a40493beb07b289f5fc8968ba89
Arthur Boesing Bilibio (DDP - Tecnologia) <arthur.bilibio@cetil.com.br>
2022-03-14 18:53:50 -0300

    Add project files.

---
+++ InAndOutNet6Training/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InAndOutNet6Training.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
