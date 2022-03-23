47782c1df1a87c88f5e68b471f0ee730a1ba2edb
Arthur Boesing Bilibio (DDP - Tecnologia) <arthur.bilibio@cetil.com.br>
2022-03-22 23:28:28 -0300

    [Controllers] Adding AppointmentController.

---
+++ InAndOutNet6Training/Controllers/AppointmentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InAndOutNet6Training.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            string todaysDate = DateTime.Now.ToShortDateString();
+            return Ok(todaysDate);
+            //return View();
+        }
+    }
+}
