67dbb0a924cd419717abedb0fc78f662561742a0
Arthur Boesing Bilibio (DDP - Tecnologia) <arthur.bilibio@cetil.com.br>
2022-10-19 14:10:52 -0300

    Creat ContactController using attribute based routing

---
+++ InAndOutNet6Training/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InAndOutNet6Training.Controllers
+{
+    [Route("Admin/[controller]")]
+    public class ContactController : Controller
+    {
+        [Route("Main")]
+        public IActionResult Index()
+        {
+            return Ok("Action main called");
+        }
+
+        [Route("Details/{id?}")]
+        public IActionResult SomeActionName(int id)
+        {
+            return Ok($"Action Details called with id: {id}");
+        }
+    }
+}
