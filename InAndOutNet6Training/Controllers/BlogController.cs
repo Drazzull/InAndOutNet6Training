afe4981e535878472715ffd9a1fe5fb24a09b7bc
Arthur Boesing Bilibio (DDP - Tecnologia) <arthur.bilibio@cetil.com.br>
2022-10-19 14:11:50 -0300

    [InAndOutNet6Training] Created a new method in the BlogController using attribute based routing.

--- InAndOutNet6Training/Controllers/BlogController.cs
+++ InAndOutNet6Training/Controllers/BlogController.cs
@@ -8,5 +8,13 @@ namespace InAndOutNet6Training.Controllers
         {
             return Ok("Action of Blog controller is called.");
         }
+
+        [Route("Blog")]
+        [Route("Blog/Index")]
+        [Route("Blog/Index/{id?}")]
+        public IActionResult AnyActionName(int id)
+        {
+            return Ok("Index called");
+        }
     }
 }
