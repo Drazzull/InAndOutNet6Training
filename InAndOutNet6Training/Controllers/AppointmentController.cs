9ea8d656ddb0068372c0e2bb45b4a20440ac9d6e
Arthur Boesing Bilibio (DDP - Tecnologia) <arthur.bilibio@cetil.com.br>
2022-10-19 15:27:03 -0300

    [InAndOutNet6Training] Including actions with parameters to AppointmentController.

--- InAndOutNet6Training/Controllers/AppointmentController.cs
+++ InAndOutNet6Training/Controllers/AppointmentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InAndOutNet6Training.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InAndOutNet6Training.Controllers
 {
@@ -12,6 +13,29 @@ namespace InAndOutNet6Training.Controllers
         public IActionResult Details(int id)
         {
             return Ok($"You have entered: {id}");
+        }
+
+        public IActionResult GetSomeData([FromQuery] string values)
+        {
+            return Ok($"The value: {values} is from Query string");
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromHeader] string parentRequestId)
+        {
+            return Ok($"Got a header with parentRequestId: {parentRequestId}");
+        }
+
+        [HttpPost]
+        public IActionResult AddApointment([FromBody] Appointment appointment)
+        {
+            return Ok(appointment);
+        }
+
+        [HttpPost]
+        public IActionResult SaveFile([FromForm] string fileName, [FromForm] IFormFile file)
+        {
+            return Ok("Success");
         }
     }
 }
---
+++ InAndOutNet6Training/Models/Appointment.cs
@@ -0,0 +1,16 @@
+﻿namespace InAndOutNet6Training.Models
+{
+    public class Appointment
+    {
+        public Appointment()
+        {
+            Description = string.Empty;
+            Name = string.Empty;
+        }
+
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public string Name { get; set; }
+    }
+}
