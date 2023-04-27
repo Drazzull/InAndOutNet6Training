a13bf6c707ee4faccb3c6fca3a82f7af78b2437e
Arthur Boesing Bilibio (DDP - Tecnologia) <arthur.bilibio@cetil.com.br>
2023-04-26 22:54:12 -0300

    [Data] Add Items table to the DBContext.

--- InAndOutNet6Training/Data/ApplicationDbContext.cs
+++ InAndOutNet6Training/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using InAndOutNet6Training.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace InAndOutNet6Training.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Item>? Items { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
