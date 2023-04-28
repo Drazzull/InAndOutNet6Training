e3d581f946c5e556509a5cfc991eb61fed85a4a0
Arthur Boesing Bilibio (DDP - Tecnologia) <arthur.bilibio@cetil.com.br>
2023-04-28 19:20:12 -0300

    [Models] Added ItemName and Lender properties to Item model class.

--- InAndOutNet6Training/Models/Item.cs
+++ InAndOutNet6Training/Models/Item.cs
@@ -4,9 +4,13 @@ namespace InAndOutNet6Training.Models
 {
     public class Item
     {
+        public string? Borrower { get; set; }
+
         [Key]
         public int Id { get; set; }
-        
-        public string? Borrower { get; set; }
+
+        public string? ItemName { get; set; }
+
+        public string? Lender { get; set; }
     }
 }
