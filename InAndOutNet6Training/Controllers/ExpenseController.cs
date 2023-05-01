4c440ee563479a00daaaeadf7d83abd2f939abc9
Arthur Boesing Bilibio (DDP - Tecnologia) <arthur.bilibio@cetil.com.br>
2023-05-01 14:17:40 -0300

    [InAndOutNet6Traininig] Creation of Expense controller and Index view.

---
+++ InAndOutNet6Training/Controllers/ExpenseController.cs
@@ -0,0 +1,29 @@
+﻿using InAndOutNet6Training.Data;
+using InAndOutNet6Training.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InAndOutNet6Training.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExpenseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                IEnumerable<Expense> expenseList = _context.Expenses;
+                return View(expenseList);
+            }
+            catch (Exception ex)
+            {
+                return View(ex.Message);
+            }
+        }
+    }
+}
