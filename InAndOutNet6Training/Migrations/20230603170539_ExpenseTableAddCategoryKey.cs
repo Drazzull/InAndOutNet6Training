4fb997e4c9681357489b9fecc7e3736546fa46fc
Arthur Boesing Bilibio (DDP - Tecnologia) <arthur.bilibio@cetil.com.br>
2023-06-03 14:10:58 -0300

    [InAndOutNet6Training] Added ExpenseCategory foreign key to Expense model, and added the migration.

---
+++ InAndOutNet6Training/Migrations/20230603170539_ExpenseTableAddCategoryKey.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InAndOutNet6Training.Migrations
+{
+    public partial class ExpenseTableAddCategoryKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExpenseCategoryId",
+                table: "Expenses",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expenses_ExpenseCategoryId",
+                table: "Expenses",
+                column: "ExpenseCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_ExpenseCategories_ExpenseCategoryId",
+                table: "Expenses",
+                column: "ExpenseCategoryId",
+                principalTable: "ExpenseCategories",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_ExpenseCategories_ExpenseCategoryId",
+                table: "Expenses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Expenses_ExpenseCategoryId",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "ExpenseCategoryId",
+                table: "Expenses");
+        }
+    }
+}
--- InAndOutNet6Training/Models/Expense.cs
+++ InAndOutNet6Training/Models/Expense.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace InAndOutNet6Training.Models
 {
@@ -7,6 +9,12 @@ namespace InAndOutNet6Training.Models
         [Required]
         [Range(0.01, double.MaxValue, ErrorMessage = "Cost must be greater than 0.00")]
         public double Cost { get; set; }
+
+        [ForeignKey(nameof(ExpenseCategoryId))]
+        public virtual ExpenseCategory? ExpenseCategory { get; set; }
+
+        [DisplayName("Expense Category")]
+        public int? ExpenseCategoryId { get; set; }
 
         [Key]
         public int Id { get; set; }
